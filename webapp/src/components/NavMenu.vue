e4d9d80dc205e2a8a41ef88c7b256fdffaacb79a
bichenghu <hbcdec@gmail.com>
2020-11-21 23:08:36 +0800

    update NavMenu

--- webapp/src/components/NavMenu.vue
+++ webapp/src/components/NavMenu.vue
@@ -1,52 +1,48 @@
 <template>
-<div>
-<el-row class="tac">
-<el-col :span="24">
+<div style="height:100%">
 <el-menu
       class="el-menu-vertical-demo"
-      style="width:200px; "
+      style="width:200px; height:100%"
       @open="handleOpen"
       @close="handleClose"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
-      <el-submenu index="1">
+      <el-menu-item  index="1">
+          <i class="el-icon-edit"></i>
+          <span>发布文章</span>
+      </el-menu-item >
+
+      <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-menu"></i>
-          <span>导航一</span>
+          <span slot="title">文章管理</span>
         </template>
-        <el-submenu index="1-1">
-          <template slot="title">选项1</template>
-          <el-menu-item index="1-1-1">选项1-1</el-menu-item>
-        </el-submenu>
-        <el-submenu index="1-2">
-          <template slot="title">选项2</template>
-          <el-menu-item index="1-2-1">选项2-1</el-menu-item>
-        </el-submenu>
-        <el-submenu index="1-3">
-          <template slot="title">选项3</template>
-          <el-menu-item index="1-3-1">选项3-1</el-menu-item>
-        </el-submenu>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
+        
+        <el-menu-item index="2-1">
+        <i class="el-icon-document"></i>
+        <span slot="title">文章列表</span>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+        <i class="el-icon-s-management"></i>
+        <span slot="title">分类管理</span>
+        </el-menu-item>
+        <el-menu-item index="2-3">
+        <i class="el-icon-discount"></i>
+        <span slot="title">标签管理</span>
+        </el-menu-item>
+
       </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
+      
       <el-menu-item index="3">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航三</span>
+        <i class="el-icon-s-data"></i>
+        <span slot="title">数据统计</span>
       </el-menu-item>
       <el-menu-item index="4">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航四</span>
+        <i class="el-icon-s-tools"></i>
+        <span slot="title">系统设置</span>
       </el-menu-item>
     </el-menu>
-</el-col>
-</el-row>
 </div>
 </template>
 
--- webapp/src/views/Admin.vue
+++ webapp/src/views/Admin.vue
@@ -1,9 +1,27 @@
 <template>
     <div id="admin">
-    <el-container>
-      <el-header class="header">
-        <vheader></vheader>
-      </el-header>
+    <el-container class="admin_container">
+      <!-- <el-header class="header">
+        <vheader>
+          
+        </vheader> -->
+      <el-header>
+        <div class="home_title">博客管理后台</div>
+        <div class="home_userinfoContainer">
+          <el-dropdown @command="handleCommand">
+            <span class="el-dropdown-link home_userinfo">
+                {{currentUserName}}<i class="el-icon-arrow-down el-icon--right home_userinfo"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="sysMsg">系统消息</el-dropdown-item>
+              <el-dropdown-item command="MyArticle">我的文章</el-dropdown-item>
+              <el-dropdown-item command="MyHome">个人主页</el-dropdown-item>
+              <el-dropdown-item command="logout" divided>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+
       <el-container>
           <navmenu></navmenu>
       </el-container>
@@ -15,26 +33,62 @@
 
 <script>
 import NavMenu from '@/components/NavMenu'
-import Header from '@/components/Header'
+//import Header from '@/components/Header'
 
 export default {
   name: 'Admin',
   data(){
       return{
-          msg: 'hello element-ui'
+          msg: 'hello element-ui',
+          currentUserName: "测试用户"
       }
   },
   components: {
     'navmenu': NavMenu,
-    'vheader': Header
+    //'vheader': Header
   }
 
 }
 </script>
 <style scoped>
-.header{
+  .admin_container {
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+  }
+
+  .el-header {
+    background-color: #20a0ff;
+    color: #333;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+
+
+/* .header{
   background-color: #545c64;
   color: #fff;
   line-height: 60px;
-}
+} */
+
+  .home_title {
+    color: #fff;
+    font-size: 22px;
+    display: inline;
+  }
+
+  .home_userinfo {
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .home_userinfoContainer {
+    display: inline;
+    margin-right: 20px;
+  }
 </style>
